f5eeeab3eaa39f770359bbd9a3b950e8e0ad428d
ElLuco6 <saintsuperylucas@gmail.com>
2022-01-25 16:50:32 +0100

    v1 version

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -41,9 +41,9 @@ export class AppComponent implements OnInit, OnDestroy {
  
   initSub() {
     this.subRoute = this.activatedRoute.queryParams.subscribe((params: any) => {
-      console.log("SUBSCRIPTION");
+     
       this.id_partner = params['id'];
-      console.log(this.id_partner);
+      
       if (this.id_partner != undefined) this.getAllBoard(this.id_partner);
       
     });
@@ -56,7 +56,7 @@ export class AppComponent implements OnInit, OnDestroy {
   
   ngOnInit() {
     
-    console.log("ON INIT");
+    
     
     
     
@@ -72,7 +72,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
     $(document).on('mouseleave', ".art_board", function () {
       $(this).find(".voir_div").slideToggle();
-      /* $(this).find(".price").toggle(500,"swing"); */
+      
       $(this).find('#spinner').toggle();
 
     });
@@ -80,14 +80,9 @@ export class AppComponent implements OnInit, OnDestroy {
     $(document).on('mouseenter', ".art_board ", function () {
 
       $(this).find(".voir_div").slideToggle();
-      /*  $(this).find(".price").toggle(500,"swing"); */
+      
     });
-    console.log('yo');
-    /* console.log(this.bio);
-     this.checkDataLength(this.bio); 
-     console.log(this.bio); */
     
-     console.log(this.spinner);
      
     
   }
@@ -109,22 +104,21 @@ export class AppComponent implements OnInit, OnDestroy {
 
       .subscribe(
         (response) => {
-          console.log(response);
+         
           this.data = response;
           this.bio = this.data.partner.bio ;
          this.spinner = false;
-         console.log(this.spinner);
+         
          
         },
         (error) => {
-          console.log(error);
+          
         }
       );
   }
 
   clickBoards(i: number) {
-    // alert(i);
-     // $( '.data_brand_model') ;
+    
      $(document).on('click', '.art_board', function () {
       let brand_model = $(this).find('.data_brand_model')[0].innerHTML;
       let rental_price = $(this).find('.price')[0].innerHTML;
@@ -134,15 +128,15 @@ export class AppComponent implements OnInit, OnDestroy {
      
      
 
-      if (board_info != undefined)   $("#popup_board_info").html('<h2 class="append_element Caveat orange fs-2 mt-3 text-uppercase fst-italic">'+board_info + '</h2>');console.log(board_info);
+      if (board_info != undefined)   $("#popup_board_info").html('<h2 class="append_element Caveat orange fs-2 mt-3 text-uppercase fst-italic">'+board_info + '</h2>');
       $("#popup_image2").html('<img src="' + image2 + '" alt="board_img" class="popup_img   m-2 " />');
       $("#popup_image1").html('<img src="' + image + '" alt="board_img"  class="popup_img   m-2 " />');
      
-      $("#popup_brand_model").html('<h2 class="append_element vert Spartan fw-bold fs-1 fw-bold mt-5 text-uppercase">'+brand_model + '</h2>');
+      $("#popup_brand_model").html('<h2 class="append_element vert Spartan fw-bold fs-1 fw-bold mt-5 text-uppercase" id="popup_board_model">'+brand_model + '</h2>');
       $("#popup_board_price").html('<h2 class="append_element vert Caveat fs-2  price"><span class="Spartan fw-bolder">'+rental_price + '</span>/H</h2>');
      
        
-   //style="height:90% ;object-fit:contain; width:100%"
+   
 
      });
      $(document).on('click', ".btnClose", function () {
@@ -170,8 +164,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
       $("#.nav-link").toggle(500, "swing");
       $('#home_nav_row').toggle(500, "swing")
-      //$("#oneBoard_overlay").fadeOut(500);
-      // $("#navbar").show();
+      
     });
 
   }
@@ -180,11 +173,15 @@ export class AppComponent implements OnInit, OnDestroy {
     $(document).on('click', ".btnClose", function () {
       $('.append_element').remove();
       $("#overlay").fadeOut(500);
+      $("#overlayMobile").fadeOut(500);
       $("#oneBoard_overlay").fadeOut(500);
       $("#navbar").show();
     });
 
 
+  }
+  mobilePopup(){
+    $("#overlayMobile").fadeIn(500);
   }
 
 
